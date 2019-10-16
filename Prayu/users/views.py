c06de597258543579c018687c073c393e9c7d7f4
Primination <primination@gmail.com>
2019-10-16 15:42:18 +0530

    Showing all products in Medicines page that are store in Database

--- Prayu/users/views.py
+++ Prayu/users/views.py
@@ -1,12 +1,26 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Product
+from math import ceil
 # Create your views here.
 
 def index(request):
+    # products = Product.objects.all()
+    # print(products)
     return render(request, 'users/index.html')
 
 def medicines(request):
-    return render(request, 'users/medicines.html')
+    allprods = []
+    catprods = Product.objects.values('category', 'product_id')
+    cats = {item['category'] for item in catprods}
+    for cat in cats:
+        prod = Product.objects.filter(category=cat)
+        n = len(prod)
+        nSlides = n // 4 + ceil((n / 4) - (n // 4))
+        allprods.append([prod, range(1, nSlides), nSlides])
+    params = {'allprods': allprods}
+    print(params)
+    return render(request, 'users/medicines.html', params)
 
 def doctors(request):
     return render(request, 'users/doctors.html')
